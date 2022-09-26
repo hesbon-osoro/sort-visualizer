82d06ca59e086c35ea0e8e96e5f87ada0f704a62
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-09-26 16:57:57 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Flex } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import Sidebar from './components/Sidebar';
+import Visualizer from './components/Visualizer';
 
 function App() {
+  const [arr, setArr] = useState([]);
+  const [generating, setGenerating] = useState(false);
+  const [sorting, setSorting] = useState(false);
+
+  const generateArr = formState => {
+    setGenerating(true);
+    setTimeout(() => {
+      let newArr = [];
+      while (newArr.length <= formState.size) {
+        let random = Math.floor(Math.random() * (200 - 10) + 10);
+        if (newArr.indexOf(random) === -1) newArr.push(random);
+      }
+      setArr([...newArr]);
+      setGenerating(false);
+    }, 500);
+  };
+  useEffect(() => {
+    generateArr({ size: 10 });
+  }, []);
+
+  const sort = () => {
+    setSorting(true);
+    setTimeout(() => {
+      let newArr = [...arr];
+      for (let i = 0; i < arr.length - 1; i++) {
+        setTimeout(() => {
+          for (let j = i + 1; j < arr.length; j++) {
+            if (newArr[i] > newArr[j]) {
+              let temp = newArr[i];
+              newArr[i] = newArr[j];
+              newArr[j] = temp;
+
+              let newStep = [...newArr];
+
+              setTimeout(() => {
+                setArr([...newStep]);
+              }, j * 100);
+            }
+            if (i === arr.length - 2) setSorting(false);
+          }
+        }, i * 1000);
+      }
+    }, 500);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box p={'4'}>
+      <Flex gap={'4'}>
+        <Sidebar
+          generating={generating}
+          sorting={sorting}
+          generateArr={generateArr}
+          sort={sort}
+        />
+        <Visualizer data={arr} />
+      </Flex>
+    </Box>
   );
 }
 
